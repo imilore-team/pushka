e67879833a29802e301e5283f8cf0933740beaf9
fede4ka1245 <bragin.fedor96@gmail.com>
2023-01-26 10:46:34 +0300

    add account page

--- src/pages/account/Account.js
+++ src/pages/account/Account.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {Grid} from "@mui/material";
+import styles from './Account.module.scss';
 
 const Account = () => {
   return (
     <>
       <Grid>
-
+        <h1 className={styles.title}>
+          HEY! Example@mail.com
+        </h1>
+        <p className={styles.text}>
+          <strong className={styles.contactUs}>
+            Contact us
+          </strong>
+          to change userinfo
+        </p>
       </Grid>
     </>
   );
